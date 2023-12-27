a39135febc0af9f89c026aaa744e297be10f5adc
Blake Warner <blakewarner@Blakes-MBP.lan>
2023-12-26 23:36:41 -0800

    added users controller and endpoints.

--- apps/store-api/src/app/users/user.controller.ts
+++ apps/store-api/src/app/users/user.controller.ts
@@ -1,28 +1,25 @@
-import { Controller, Get, Body, Put, Param, Delete, Inject, UseInterceptors, HttpStatus, HttpCode } from '@nestjs/common';
+import { Controller, Get, Body, Put, Param, Delete, Inject, HttpStatus, HttpCode } from '@nestjs/common';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserService } from './user.service';
 import { Cache } from "cache-manager";
 import * as CONSTANTS from '../shared/constants';
-import { CACHE_MANAGER, CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
+import { CACHE_MANAGER} from '@nestjs/cache-manager';
 import { EventEmitter2 } from "@nestjs/event-emitter";
 
 const rootPath = CONSTANTS.versions; // /v1
 
 @Controller(rootPath)
-export class ProductController {
+export class UserController {
     constructor(
         @Inject(CACHE_MANAGER) private cacheManager: Cache,
         private userService: UserService,
         private eventEmitter: EventEmitter2
         ){}
 
-    @HttpCode(HttpStatus.OK)
-    @CacheKey('users_frontend')
-    @CacheTTL(30 * 60)
-    @UseInterceptors(CacheInterceptor)
     @Get('users')
     findAll() {
         const users = this.userService.find();
+        console.log("users from findall ", users);
         return users;
     }
 
--- apps/store-api/src/app/users/user.module.ts
+++ apps/store-api/src/app/users/user.module.ts
@@ -3,9 +3,11 @@ import { UserService } from './user.service';
 import { DatabaseModule } from '../database/database.module';
 import { SharedModule } from '../shared/shared.module';
 import { userProviders } from './user.providers';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [DatabaseModule, SharedModule],
+  controllers: [UserController],
   providers: [UserService, ...userProviders],
   exports: [UserService]
 })
